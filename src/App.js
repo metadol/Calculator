a2673497adef8b097d57f63ea95f3014ff1efd3a
Kevin Abraham <138606225+metadol@users.noreply.github.com>
2023-11-30 14:31:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -111,6 +111,7 @@ export default App
 
 // const sliced1 = fruits.slice(1, 3); // Extracts elements from index 1 to 2 (['banana', 'cherry'])
 // const sliced2 = fruits.slice(2);    // Extracts elements from index 2 to the end (['cherry', 'date', 'fig'])
+// const sliced2 = fruits.slice(-2);    // Extracts elements from second last index to the end (['date', 'fig'])
 // const sliced3 = fruits.slice(-3, -1); // Extracts elements from the third-to-last to the last (['cherry', 'date'])
 //a new array containing elements starting from the third-to-last element up to, but not including, the last element
 
@@ -122,3 +123,6 @@ export default App
 // Negative indices are a way to reference elements in an array counting from the end of the array, 
 //with -1 representing the last element, 
 //-2 representing the second-to-last element, and so on. 
+//okay about the slice imm still unable to get the last charater using slice(-3,-1) as it does not include last elemnt even thought -1 means the lst element i
+//it does not include that so how will i ge the last element? 
+// use slice(-3) this will give you the last 3 elements if you only want one use slice(-1)
